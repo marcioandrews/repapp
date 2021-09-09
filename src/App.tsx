38b18ce7b21203f7d4c54c54297a3df1b6a8a05d
unknown <marcioasordonez@gmail.com>
2021-09-09 18:09:22 -0300

    feat: web app inicial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/landing.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img className={logoImg} alt="Repapp"/>
+        <h1>O Repapp ajuda os times a agilizar o trabalho.</h1>
+
+      </div>
     </div>
   );
 }
