4b84cb89dc8d4d404f3a68b556a200ca9851eaaa
unknown <marcioasordonez@gmail.com>
2021-09-11 16:04:50 -0300

    structure done.

---
+++ web/src/routes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path="/" component={Landing} />
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
